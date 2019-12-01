cc30a163ae95fa045e39859d0e006471d47a8db0
Tanin Thanasopon <>
2019-12-01 14:37:27 +0700

    Delete Note

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { removeNote } from '../redux/actions/actions'
 
 class NotesList extends React.Component {
   constructor(props) {
@@ -14,7 +15,7 @@ class NotesList extends React.Component {
           {this.props.notes.map(note => (
             <li key={note.id}>
               <b>{note.title}</b>
-              <button>x</button>
+              <button onClick={() => this.props.removeNote(note.id)}>x</button>
               <br />
               <span>{note.content}</span>
             </li>
@@ -32,4 +33,8 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps, null)(NotesList)
+const mapDispatchToProps = {
+  removeNote: removeNote
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NotesList)
